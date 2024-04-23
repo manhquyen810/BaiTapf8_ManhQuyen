d06269a84c3f5881a8c4fe25e7b4001b75683f04
Quyen Nguyen <nguyenmanhquyen2712@gmail.com>
2024-04-24 00:40:15 +0700

    Nop bai tap buoi 18

--- day-18/js/script.js
+++ day-18/js/script.js
@@ -2,26 +2,23 @@
 
 const btn = document.getElementById("btn-ex-1");
 btn.addEventListener("click", () => {
-  //praseInt ép kiểu số nguyên
-  var a = parseInt(document.getElementById("number-a").value);
+  var a = parseFloat(document.getElementById("number-a").value);
   var price = 0;
   if (a <= 1) {
     price = 15000;
   } else if (a > 1 && a <= 5) {
-    price = 13500;
+    price = 15000 + (a - 1) * 13500;
   } else if (a > 5) {
-    price = 11000;
-  }
-
-  var total = price * a;
-  if (a > 120) {
-    total *= 0.9;
+    price = 15000 + 4 * 13500 + (a - 5) * 11000;
+    if (a > 120) {
+      price = price * (1 - 0.1);
+    }
   }
 
   if (isNaN(a)) {
     alert("Vui lòng nhập số km");
   } else {
-    alert("Tiền cước taxi: " + total + "đ");
+    alert("Tiền cước taxi: " + price + "đ");
   }
 });
 
@@ -31,30 +28,34 @@ btnTwo.addEventListener("click", () => {
   var a = parseFloat(document.getElementById("number-a-ex-2").value);
   var price = 0;
   if (a >= 0 && a <= 50) {
-    price = 1678;
+    price = 1678 * a;
   } else if (a >= 51 && a <= 100) {
-    price = 1734;
+    price = (a - 50) * 1734 + 50 * 1678;
   } else if (a >= 101 && a <= 200) {
-    price = 2014;
+    price = (a - 100) * 2014 + 50 * 1678 + 50 * 1734;
   } else if (a >= 201 && a <= 300) {
-    price = 2536;
+    price = (a - 200) * 2536 + 100 * 2014 + 50 * 1678 + 50 * 1734;
   } else if (a >= 301 && a <= 400) {
-    price = 2834;
+    price = (a - 300) * 2834 + 100 * 2536 + 100 * 2014 + 50 * 1678 + 50 * 1734;
   } else {
-    price = 2927;
+    price =
+      (a - 400) * 2927 +
+      100 * 2834 +
+      100 * 2536 +
+      100 * 2014 +
+      50 * 1678 +
+      50 * 1734;
   }
 
-  var total = a * price;
   if (isNaN(a)) {
     alert("Vui lòng nhập số kWh");
   } else {
-    alert("Số tiền điện phải trả " + total + "đ");
+    alert("Số tiền điện phải trả " + price + "đ");
   }
 });
 
 const btnThree = document.getElementById("btn-ex-3");
 btnThree.addEventListener("click", () => {
-  //praseInt ép kiểu số nguyên
   var n = parseInt(document.getElementById("number-a-ex-3").value);
   var s = 0;
   for (var i = 1; i <= n; i++) {
@@ -71,7 +72,6 @@ btnThree.addEventListener("click", () => {
 //ex04
 const btnFour = document.getElementById("btn-ex-4");
 btnFour.addEventListener("click", () => {
-  //praseInt ép kiểu số nguyên
   var n = parseInt(document.getElementById("number-a-ex-4").value);
   function isPrime(n) {
     if (n == 1) {
@@ -97,10 +97,28 @@ btnFour.addEventListener("click", () => {
 //ex05
 const btnFive = document.getElementById("btn-ex-5");
 btnFive.addEventListener("click", () => {
-  //praseInt ép kiểu số nguyên
   var n = parseInt(document.getElementById("number-a-ex-5").value);
 
+  var count = 1;
   for (var i = 1; i <= n; i++) {
-    for (var j = 1; j <= n; j++) {}
+    for (var j = 1; j <= i; j++) {
+      document.write(count + " ");
+      count++;
+    }
+    document.write("<br>");
+  }
+});
+
+//ex06
+const btnSix = document.getElementById("btn-ex-6");
+btnSix.addEventListener("click", () => {
+  var n = 8;
+
+  for (var i = 1; i <= n; i++) {
+    for (var j = 1; j <= n; j++) {
+      document.write(count + " ");
+      count++;
+    }
+    document.write("<br>");
   }
 });
