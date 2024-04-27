8a5701988a7ca787a4f962b8f828289ca165692a
Quyen Nguyen <nguyenmanhquyen2712@gmail.com>
2024-04-27 23:25:21 +0700

    Nop bai tap buoi 19

---
+++ day-19/js/script.js
@@ -0,0 +1,7 @@
+//ex01
+const btn = document.getElementById("btn-ex-1");
+btn.addEventListener("click", () => {});
+const btnTwo = document.getElementById("btn-ex-2");
+btnTwo.addEventListener("click", () => {});
+const btnThree = document.getElementById("btn-ex-3");
+btnThree.addEventListener("click", () => {});
