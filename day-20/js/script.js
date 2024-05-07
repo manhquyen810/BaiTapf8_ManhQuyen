b88c8511e62a05607b516519b07b094525c4b9bf
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-05-07 23:34:01 +0700

    Nop bai tap buoi 20

---
+++ day-20/js/script.js
@@ -0,0 +1,19 @@
+var text =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.";
+// document.body.innerHTML = text;
+
+// function displayText() {
+//   var word = "";
+//   while (text[index] !== " " && index < text.length) {
+//     word += text[index];
+//     index++;
+//   }
+//   document.getElementById("word").innerHTML += "<span>" + word + " </span>";
+//   index++;
+// }
+
+// if (index < text.length) {
+//   setTimeout(displayText, 100); // Thay đổi số 100 để điều chỉnh tốc độ chạy chữ
+// }
+
+// displayText();
