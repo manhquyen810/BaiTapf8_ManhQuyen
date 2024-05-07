2ea8054a8a417a9a147e9ab79aac19d391f73682
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-05-08 01:58:56 +0700

    Nop bai tap buoi 20

--- day-20/js/script.js
+++ day-20/js/script.js
@@ -1,19 +1,12 @@
-var text =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.";
+var text = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500.`;
 // document.body.innerHTML = text;
 
-// function displayText() {
-//   var word = "";
-//   while (text[index] !== " " && index < text.length) {
-//     word += text[index];
-//     index++;
-//   }
-//   document.getElementById("word").innerHTML += "<span>" + word + " </span>";
-//   index++;
-// }
-
-// if (index < text.length) {
-//   setTimeout(displayText, 100); // Thay đổi số 100 để điều chỉnh tốc độ chạy chữ
-// }
-
-// displayText();
+var index = 0;
+setInterval(function () {
+  var space = text.indexOf(" ", index);
+  console.log(space);
+  var word = text.slice(index, space);
+  var wordChange = text.replace(word, `<span>${word}</span>`);
+  document.body.innerHTML = wordChange;
+  index = space + 1;
+}, 500);
