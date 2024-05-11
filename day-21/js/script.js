45ea08bdb83a5ecdb8939bdad59c905d42435d6b
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-05-12 01:42:48 +0700

    Nop bai tap buoi 21

--- day-21/js/script.js
+++ day-21/js/script.js
@@ -20,12 +20,8 @@ btn.addEventListener("click", () => {
       minIndex = +index;
     }
   }
-  console.log(
-    "Phần tử lớn nhất: " + max + ", nằm ở vị trí số " + (maxIndex + 1)
-  );
-  console.log(
-    "Phần tử nhỏ nhất: " + min + ", nằm ở vị trí số " + (minIndex + 1)
-  );
+  console.log("Phần tử lớn nhất: " + max + ", nằm ở vị trí số " + maxIndex);
+  console.log("Phần tử nhỏ nhất: " + min + ", nằm ở vị trí số " + minIndex);
 });
 
 //ex02
@@ -54,7 +50,10 @@ btnTwo.addEventListener("click", () => {
     }
   }
   if (prime) {
-    console.log("Trung bình các số nguyên tố trong mảng: ", sum / count);
+    console.log(
+      "Trung bình các số nguyên tố trong mảng: ",
+      parseFloat(sum / count)
+    );
   } else {
     console.log("Không có số nguyên tố trong mảng");
   }
@@ -63,12 +62,49 @@ btnTwo.addEventListener("click", () => {
 //ex03
 const btnThree = document.getElementById("btn-ex-3");
 btnThree.addEventListener("click", () => {
-  var arr = [5, 2, 5, 2, 1, 9, 6, 1];
+  var arr = [5, 2, 5, 2, 1, 9, 6, 1, "Quyền", "Quyền"];
   var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    if (arr.indexOf(arr[i]) === i) {
-      newArr[newArr.length] = arr[i];
+  for (var i of arr) {
+    var repeat = false;
+    for (var j of newArr) {
+      if (i === j) {
+        repeat = true;
+        break;
+      }
+    }
+    if (!repeat) {
+      newArr[newArr.length] = i;
     }
   }
   console.log(newArr);
+});
+
+//ex04
+const btnFour = document.getElementById("btn-ex-4");
+btnFour.addEventListener("click", () => {
+  var numbers = [5, 1, 9, 8, 10];
+  var element = 4;
+  var newNumbers = [];
+
+  function swap(arr, index1, index2) {
+    var temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+  }
+  function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+      var min = i;
+      for (var j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[min]) {
+          min = j;
+        }
+      }
+      swap(arr, i, min);
+    }
+    return arr;
+  }
+  var result = selectionSort(numbers);
+  console.log("Bước 1: ", result);
+  numbers[numbers.length] = element;
+  console.log(numbers);
 });
