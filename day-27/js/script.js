5a93909f45e46ee62339e6451b39b79cf656d92c
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-06-02 02:37:28 +0700

    Nop bai tap buoi 27

--- day-27/js/script.js
+++ day-27/js/script.js
@@ -1,39 +1,63 @@
-//Show - close
 var press = document.querySelector("#press");
 var login = document.querySelector(".login");
-var closeBtn = document.querySelector(".close");
-var overlay = document.querySelector(".login-overlay");
-var notifyEmail = document.querySelector(".notify-email");
-var notifyPass = document.querySelector(".notify-pass");
+var register = document.querySelector(".register");
+
+var btnLogin = document.querySelectorAll(".btn-login");
+var btnRes = document.querySelectorAll(".btn-register");
+
+var closeButtons = document.querySelectorAll(".close");
+var overlayLog = document.querySelector(".overlay");
+var overlayRes = document.querySelector(".overlay-2");
+
+var notifyEmails = document.querySelectorAll(".notify-email");
+var notifyPass = document.querySelectorAll(".notify-pass");
 
 press.addEventListener("click", function () {
   login.classList.add("show");
 });
 
-closeBtn.addEventListener("click", function () {
-  login.classList.remove("show");
-  notifyEmail.innerHTML = "";
-  notifyPass.innerHTML = "";
+closeButtons.forEach(function (button) {
+  //   console.log(button);
+  button.addEventListener("click", function () {
+    closeForm();
+  });
 });
 
-overlay.addEventListener("click", function () {
-  login.classList.remove("show");
-  notifyEmail.innerHTML = "";
-  notifyPass.innerHTML = "";
+overlayLog.addEventListener("click", function () {
+  closeForm();
 });
 
-//validate
+overlayRes.addEventListener("click", function () {
+  closeForm();
+});
+
+btnRes.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    register.classList.add("show-2");
+    login.classList.remove("show");
+  });
+});
+
+btnLogin.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    login.classList.add("show");
+    register.classList.remove("show-2");
+  });
+});
+
+function closeForm() {
+  login.classList.remove("show");
+  register.classList.remove("show-2");
+  overlayLog.classList.remove("show");
+  overlayRes.classList.remove("show-2");
+  //   overlay.classList.remove("show-2");
+}
+
 var email = document.querySelector(".import-email");
 var pass = document.querySelector(".import-pass");
 var importInfor = document.querySelector(".log");
 
-importInfor.addEventListener("click", function () {
-  //   console.log(email.value);
-  login.addEventListener("click", function (e) {
-    if (email.value === "" || pass.value === "") {
-      notifyEmail.innerHTML = "Vui lòng nhập thông tin";
-      notifyPass.innerHTML = "Vui lòng nhập thông tin";
-    }
-    console.log(e);
-  });
-});
+function clearNotifications() {
+  notifyEmail.innerHTML = "";
+  notifyPass.innerHTML = "";
+}
