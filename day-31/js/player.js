8e7c507736dd64de5cbfbf9e7c325d3b8026cc07
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-06-16 00:48:11 +0700

    Nop bai tap buoi 31

--- day-31/js/player.js
+++ day-31/js/player.js
@@ -1,78 +1,62 @@
 var progressBar = document.querySelector(".progress-bar");
-var progress = document.querySelector(".progress");
-var progressSpan = document.querySelector("span");
+var progress = progressBar.querySelector(".progress");
+var progressSpan = progress.querySelector("span");
 
-//Bấm chuột xuống tại progress-bar =>Chấm màu tím di chuyeenrv tới vị trí vừa bấm
-var offsetX = 0,
-  initialClientX = 0;
+//Bấm chuột xuống tại progress-bar ==> Chấm màu tím sẽ di chuyển tới vị trí vừa bấm
+
+//Tính chiều rộng của progess-bar
 var progressBarWidth = progressBar.clientWidth;
-var offsetProgressBar = 0;
+var offsetX = 0;
+var initialClientX = 0;
 var lastOffsetProgressBar = 0;
-
+var offsetProgressBar = 0;
 progressBar.addEventListener("mousedown", function (e) {
   //Lấy được tọa độ tại vị trí bấm (offsetX)
   offsetX = e.offsetX;
-  console.log(offsetX);
-  //Tính chiều rộng của progress-bar
 
   //Tính tỷ lệ phần trăm giữa tọa độ bấm xuống và chiều rộng
   var rate = (offsetX / progressBarWidth) * 100;
+  //Cập nhật CSS vào progress
   progress.style.width = rate + "%";
   lastOffsetProgressBar = offsetX;
   offsetProgressBar = offsetX;
   initialClientX = e.clientX;
-  audio.currentTime = (rate / 100) * duration;
-  document.addEventListener("mousemove", handlerDrag);
+  document.addEventListener("mousemove", handleDrag);
 });
 
 progressSpan.addEventListener("mousedown", function (e) {
   e.stopPropagation();
-  document.addEventListener("mousemove", handlerDrag);
-  //   var offsetSpan = e.offsetX;
-  //   var rate = ((offsetX + offsetSpan / 2) / progressBarWidth) * 100;
-  //   progress.style.width = rate + "%";
-  initialClientX = e.clientX; //gán vị trị của button tím so với body
+  document.addEventListener("mousemove", handleDrag);
+  initialClientX = e.clientX; //Gán ví trí của button màu tím so với body
 });
-
 document.addEventListener("mouseup", function () {
-  document.removeEventListener("mousemove", handlerDrag);
+  document.removeEventListener("mousemove", handleDrag);
   lastOffsetProgressBar = offsetProgressBar;
 });
-
-var handlerDrag = function (e) {
+var handleDrag = function (e) {
   var clientX = e.clientX;
-  console.log(lastOffsetProgressBar);
   offsetProgressBar = clientX - initialClientX + lastOffsetProgressBar;
   var rate = (offsetProgressBar / progressBarWidth) * 100;
+  if (rate < 0) {
+    rate = 0;
+  }
   if (rate > 100) {
     rate = 100;
   }
-  if (rate < 100) {
-    rate = 0;
-  }
   progress.style.width = rate + "%";
-  //   console.log(offsetProgressBar);
   audio.currentTime = (rate / 100) * duration;
 };
 
-var audio = document.querySelector(".audio");
+var audio = document.querySelector("audio");
 var player = document.querySelector(".player");
-var playerBtn = document.querySelector(".play-btn i");
-var playTimer = document.querySelector(".play-timer");
-var runTimer = document.querySelector(".run-timer");
-var runEl = document.querySelector(".run-timer span");
-
+var playBtn = player.querySelector(".play-btn i");
+var playTimer = player.querySelector(".play-timer");
 var currentTimeEl = playTimer.firstElementChild;
 var durationEl = playTimer.lastElementChild;
-
-console.log(currentTimeEl);
-console.log(durationEl);
-
 var duration = 0;
 var setDuration = function () {
   duration = audio.duration;
 };
-
 var getTimeFormat = function (seconds) {
   var mins = Math.floor(seconds / 60);
   seconds = Math.floor(seconds - mins * 60);
@@ -80,58 +64,43 @@ var getTimeFormat = function (seconds) {
     seconds < 10 ? "0" + seconds : seconds
   }`;
 };
-
-// console.log(audio);
 window.addEventListener("load", function () {
-  // console.log("load");
   setDuration();
-  // console.log(getTimeFormat(duration));
   durationEl.innerText = getTimeFormat(duration);
-  playerBtn.addEventListener("click", function () {
-    // console.log("clicked");
+  playBtn.addEventListener("click", function () {
     if (audio.paused) {
       //Nhạc đang dừng
-      audio.play(); //phát nhạc
-      // this.classList.replace("fa-play", "fa-pause");
+      audio.play(); //Phát nhạc
     } else {
       //Nhạc đang phát
-      audio.pause(); //dừng nhạc
-      // this.classList.replace("fa-pause", "fa-play");
+      audio.pause(); //Dừng nhạc
     }
-
-    // console.log(audio.paused);
   });
-
   audio.addEventListener("play", function () {
-    playerBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.classList.replace("fa-play", "fa-pause");
   });
   audio.addEventListener("pause", function () {
-    playerBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.classList.replace("fa-pause", "fa-play");
   });
   audio.addEventListener("timeupdate", function () {
     var currentTime = audio.currentTime;
-    // console.log(currentTime);
     currentTimeEl.innerText = getTimeFormat(currentTime);
-    // console.log(currentTimeEl);
     var rate = (currentTime / duration) * 100;
-    // console.log(rate);
     progress.style.width = `${rate}%`;
   });
-});
 
-//click để đến chỗ nghe dần
-var progressBarWidth = progressBar.clientWidth;
-console.log(progressBarWidth);
-console.log(setDuration());
-progressBar.addEventListener("click", function (e) {
-  offsetX = e.offsetX;
-  //   console.log(offsetX);
-  var newTime = (offsetX / progressBarWidth) * duration;
-  audio.currentTime = newTime;
-});
+  //click để đến chỗ nghe dần
+  progressBar.addEventListener("click", function (e) {
+    offsetX = e.offsetX;
+    //   console.log(offsetX);
+    var newTime = (offsetX / progressBarWidth) * audio.duration;
+    //   console.log(duration);
+    audio.currentTime = newTime;
+  });
 
-//reset audio
-audio.addEventListener("ended", function () {
-  audio.currentTime = 0;
-  //   console.log(audio.currentTime);
+  //reset audio
+  audio.addEventListener("ended", function () {
+    audio.currentTime = 0;
+    //   console.log(audio.currentTime);
+  });
 });
