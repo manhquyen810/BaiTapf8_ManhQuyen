742af3d07d5f00232228e49e530278a2dbb275e6
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-07-03 01:02:06 +0700

    Nop bai tap buoi 36

--- day-36/js/getlink.js
+++ day-36/js/getlink.js
@@ -1,3 +1,46 @@
-let container = document.querySelector(".container");
-let time = container.querySelector(".time");
-let btn = container.querySelector(".btn");
+const time = document.querySelector(".time");
+const btn = document.querySelector(".btn");
+
+let count = 10;
+let lastTime = Date.now();
+let flag = true;
+
+function updateTimer() {
+  if (!flag) return;
+
+  let currentTime = Date.now();
+  let passTime = (currentTime - lastTime) / 1000;
+  //   console.log(passTime);
+  lastTime = currentTime;
+
+  count -= passTime;
+  //   console.log(count);
+  if (count <= 0) {
+    count = 0;
+    btn.disabled = false;
+    btn.classList.add("active");
+    btn.style.cursor = "pointer";
+  }
+
+  time.textContent = Math.ceil(count);
+
+  if (count > 0) {
+    requestAnimationFrame(updateTimer);
+  }
+}
+
+document.addEventListener("visibilitychange", () => {
+  flag = !document.hidden;
+  if (flag) {
+    lastTime = Date.now();
+    requestAnimationFrame(updateTimer);
+  }
+});
+
+btn.addEventListener("click", () => {
+  if (count <= 0) {
+    window.location.href = "https://fullstack.edu.vn/";
+  }
+});
+
+requestAnimationFrame(updateTimer);
