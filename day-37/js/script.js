641c4b44f2645d6d7e36b22487be3a283849dbd0
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-07-09 23:53:05 +0700

    Nop bai tap buoi 38

--- day-37/js/script.js
+++ day-37/js/script.js
@@ -1,31 +1,3 @@
-class F8 {
-  static components = {};
-  static component(name, obj) {
-    // console.log(name, option);
-    this.components[name] = obj;
-    class Component extends HTMLElement {
-      constructor() {
-        //Khởi tạo customEl
-        super();
-        this.attachShadow({ mode: "open" });
-        if (obj.data) {
-          this.state = obj.data();
-        }
-      }
-      connectedCallback() {
-        var _this = this;
-        _this.render();
-        _this.addEventListener();
-      }
-      render() {
-        let template = obj.template;
-        console.log(template);
-      }
-    }
-    customElements.define(name, Component);
-  }
-}
-
 F8.component("counter-app", {
   data: () => {
     return {
--- day-37/js/test.js
+++ day-37/js/test.js
@@ -1,1 +1,27 @@
-
+class F8 {
+  static components = {};
+  static component(name, obj) {
+    // console.log(name, option);
+    this.components[name] = obj;
+    class Component extends HTMLElement {
+      constructor() {
+        //Khởi tạo customEl
+        super();
+        this.attachShadow({ mode: "open" });
+        if (obj.data) {
+          this.state = obj.data();
+        }
+      }
+      connectedCallback() {
+        var _this = this;
+        _this.render();
+        _this.addEventListener();
+      }
+      render() {
+        let template = obj.template;
+        console.log(template);
+      }
+    }
+    customElements.define(name, Component);
+  }
+}
