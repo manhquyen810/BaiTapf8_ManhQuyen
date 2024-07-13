c15e199231578289c8a17e62fb27a77445f3be15
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-07-14 02:12:16 +0700

    Nop bai tap buoi 39

--- day-39/js/blog.js
+++ day-39/js/blog.js
@@ -1,47 +1,64 @@
 const serverApi = `http://localhost:3000`;
+let page = 1;
 const getBlogs = async () => {
-  const response = await fetch(`${serverApi}/blogs`);
-  const blogs = await response.json();
-  renderBlog(blogs);
-  //   testBlog(blogs);
-  console.log(blogs);
+  try {
+    const response = await fetch(`${serverApi}/blogs?page=${page}`);
+    const blogs = await response.json();
+    renderBlog(blogs);
+    //   testBlog(blogs);
+    console.log(blogs);
+    page++;
+  } catch (e) {
+    console.log(e.message);
+  }
 };
 // getBlogs();
 
 const renderBlog = (blogs) => {
   const content = document.querySelector(".content");
-  content.innerHTML = `${blogs
+  const blogsInfor = blogs
     .map(({ author, time, content, links }) => {
       return `<section class="blog mt-6">
-            <div class="time">
-              <span class="days">${time.days} ngày trước</span>
-              <div class="clock">
-                <span class="hours">${time.hour}</span
-                ><span class="mins">${time.min}</span>
+              <div class="time">
+                <span class="days">${time.days} ngày trước</span>
+                <div class="clock">
+                  <span class="hours">${time.hour}</span
+                  ><span class="mins">${time.min}</span>
+                </div>
               </div>
-            </div>
-            <div class="blog-content">
-              <div class="blog-name">
-                <span class="circle"><a href="#" class="alpha">${
-                  author.avatar
-                }</a></span>
-                <a href="#" class="name">${author.username}</a>
+              <div class="blog-content">
+                <div class="blog-name">
+                  <span class="circle"><a href="#" class="alpha">${
+                    author.avatar
+                  }</a></span>
+                  <a href="#" class="name">${author.username}</a>
+                </div>
+                <h2>${content.title}</h2>
+                <p>${content.body}</p>
+                ${links
+                  .map(
+                    (link) => `<span class="link">
+                  <span class="hashtag">#</span>
+                  <a href="#" class="link-infor">${link.text}</a>
+                </span>`
+                  )
+                  .join("")}
+                <hr style="width: 100%" />
               </div>
-              <h2>${content.title}</h2>
-              <p>${content.body}</p>
-              ${links
-                .map(
-                  (link) => `<span class="link">
-                <span class="hashtag">#</span
-                ><a href="#" class="link-infor">${link.text}</a></span
-              >`
-                )
-                .join("")}
-              <hr style="width: 100%" />
-            </div>
-          </section>`;
+            </section>`;
     })
-    .join("")}`;
+    .join("");
+
+  content.innerHTML += blogsInfor;
 };
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    getBlogs();
+  }
+});
 
 getBlogs();
