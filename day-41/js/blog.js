6d0b7e0025668b3ebdc8a3aaa623b23c8dee3e99
manhquyen810 <nguyenmanhquyen2712@gmail.com>
2024-07-20 23:36:43 +0700

    Nop bai tap buoi 41

---
+++ day-41/js/blog.js
@@ -0,0 +1,64 @@
+const serverApi = `https://tdnmqx-8080.csb.app`;
+let page = 1;
+const getBlogs = async () => {
+  try {
+    const response = await fetch(`${serverApi}/blogs?page=${page}`);
+    const blogs = await response.json();
+    renderBlog(blogs);
+    //   testBlog(blogs);
+    console.log(blogs);
+    page++;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+// getBlogs();
+
+const renderBlog = (blogs) => {
+  const content = document.querySelector(".content");
+  const blogsInfor = blogs
+    .map(({ author, time, content, links }) => {
+      return `<section class="blog mt-6">
+              <div class="time">
+                <span class="days">${time.days} ngày trước</span>
+                <div class="clock">
+                  <span class="hours">${time.hour}</span
+                  ><span class="mins">${time.min}</span>
+                </div>
+              </div>
+              <div class="blog-content">
+                <div class="blog-name">
+                  <span class="circle"><a href="#" class="alpha">${
+                    author.avatar
+                  }</a></span>
+                  <a href="#" class="name">${author.username}</a>
+                </div>
+                <h2>${content.title}</h2>
+                <p>${content.body}</p>
+                ${links
+                  .map(
+                    (link) => `<span class="link">
+                  <span class="hashtag">#</span>
+                  <a href="#" class="link-infor">${link.text}</a>
+                </span>`
+                  )
+                  .join("")}
+                <hr style="width: 100%" />
+              </div>
+            </section>`;
+    })
+    .join("");
+
+  content.innerHTML += blogsInfor;
+};
+
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    getBlogs();
+  }
+});
+
+getBlogs();
